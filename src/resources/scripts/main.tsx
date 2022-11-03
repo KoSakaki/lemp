5a87a0105d1dbf5d6b6561e3935f18a17c035226
KoSakaki <sakaki@flaginc.net>
2022-11-03 17:06:24 +0900

    add react-ts

---
+++ src/resources/scripts/main.tsx
@@ -0,0 +1,13 @@
+// Build something cool. <3
+import React from "react";
+import * as ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
