ad1f91635bd226f6d2f4faa73be713ac612304fa
KoSakaki <sakaki@flaginc.net>
2022-11-03 19:04:16 +0900

    index.blade.phpにgフロントを一任

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 Route::get('/fuga', function () {
-    return view('about');
+    return view('index');
 });
